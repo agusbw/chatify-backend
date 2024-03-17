9aa1d2bb8ee1beedf38d0feb8f647fc0ebe91123
agusbw <agusbudhiarta81@gmail.com>
2024-03-17 17:34:44 +0800

    Refactor user validation in user-validation.ts

--- src/validations/user-validation.ts
+++ src/validations/user-validation.ts
@@ -2,21 +2,16 @@ import { z } from "zod";
 
 export const register = z
   .object({
-    password: z
-      .string({
-        required_error: "Password is required",
-      })
-      .min(6, "Password must be at least 6 characters"),
+    password: z.string().min(6, "Password must be at least 6 characters."),
     username: z
-      .string({
-        required_error: "Username is required",
-      })
-      .min(2, "Username must be at least 2 characters"),
+      .string()
+      .min(2, "Username must be at least 2 characters.")
+      .refine((v) => {
+        return /^[a-z0-9_]+$/.test(v);
+      }, "Username can only contain lowercase letters, numbers, and underscores."),
     confirmPassword: z
-      .string({
-        required_error: "Confirm password is required",
-      })
-      .min(6, "Password must be at least 6 characters"),
+      .string()
+      .min(6, "Password must be at least 6 characters."),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
