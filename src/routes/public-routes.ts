6dad989c706207a417095c634f1b2a7f232aa8b3
agus_bw <agusbudhiarta81@gmail.com>
2024-05-14 20:32:17 +0800

    chore: welcome message :V

--- src/routes/public-routes.ts
+++ src/routes/public-routes.ts
@@ -3,6 +3,12 @@ import * as userController from "../controllers/user-controller";
 
 const publicRoute: Router = express.Router();
 
+publicRoute.get("/", (req, res) => {
+  return res.json({
+    message: "Visit the site at https://chatify-frontend-ten.vercel.app/",
+    contact_me: "Twitter: @agusbw_ ; IG: @agus_bw",
+  });
+});
 publicRoute.post("/api/register", userController.registerUser);
 publicRoute.post("/api/login", userController.loginUser);
 
